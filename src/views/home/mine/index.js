c77d553dbe7b913b5be52ce8bad4b3f429fcd388
xilou <xilou@qq.com>
2019-10-31 08:36:42 +0800

    xilou_v1.01

--- src/views/home/mine/index.js
+++ src/views/home/mine/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './index.scss'
+import { Route} from "react-router-dom"
+import Personal from "./personal";
+import {Information, Tree, Tree1, User} from "./userele";
 
 function Mine() {
     return (
-        <div>
-            Mine
+        <div className="mine-box">
+            <Personal/>
         </div>
     )
 }
---
+++ src/views/home/mine/userele/userlist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Userlist = props => {
+    return (
+        <div>
+        长列表
+        </div>
+    );
+}
+
+export default Userlist;
